0f5026ca01db599a3cda4c85fd24ff45f29a3853
Sam Hennessy <samuelhennessy0@gmail.com>
2020-10-13 16:01:52 +0100

    Lab 3 Upload

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export class Footer extends React.Component {
+
+    render() {
+        return (
+
+            <div>
+                <h1>This is the footer Component</h1>
+
+            </div>
+        );
+
+
+    }
+
+
+}
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export class Header extends React.Component {
+
+    render() {
+        return (
+
+            <div>
+                <h1>This is the header Component</h1>
+
+            </div>
+        );
+
+
+    }
+
+
+}
